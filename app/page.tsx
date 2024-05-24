90f224014de1f6d59589e5c57a36ee0db7ef1939
Elliott Fouts <elliott@thisdot.co>
2024-05-24 00:13:23 -0400

    Reduce home links to just two examples

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@ export default function Page() {
   return (
     <>
       <p>
-        <Link href="/checkout/create">Checkout</Link>
-      </p>
-      <p>
-        <Link href="/checkout-using-params?step=create">
-          Checkout using params
-        </Link>
-      </p>
-      <p>
         <Link href="/server-action-with-layout-suspense/page">
           Server action with layout suspense
         </Link>
